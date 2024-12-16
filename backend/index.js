c05ec9972ab7cd4e0a983f39198070c84f2d788f
Om Kariya <161995427+odk1572@users.noreply.github.com>
2024-12-17 03:22:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://edupulse-m37g.onrender.com/profile",
     credentials:true
 }));
  
